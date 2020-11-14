010ee91eedf56da0e9af8578852aeaa140c0fecf
Amir Sol <amir_sol@outlook.com>
2020-11-13 17:09:52 -0800

    first iteration of browse

---
+++ browse.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Browse</title>
+    <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700&family=Fauna+One&display=swap" rel="stylesheet">
+    <link href="css/browse.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/normalize.css">
+
+    <!-- Took a little help from online resource: https://tutorialzine.com/-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+<div class="header">
+    <div class="navcontainer">
+        <nav>
+            <input id="nav-toggle" type="checkbox">
+            <div class="logo logoCo"><a href="index.html" class="logoCo"><strong>VELP</strong></a></div>
+            <ul class="links">
+                <li><a href="index.html">Home</a></li>
+                <li><a href="browse.html">Browse</a></li>
+                <li><a href="member.html">Account</a></li>
+                <li><a href="account.html">Sign-up</a></li>
+
+            </ul>
+            <label for="nav-toggle" class="icon-burger">
+                <span class=" c line"></span>
+                <span class=" c line"></span>
+                <span class=" c line"></span>
+            </label>
+        </nav>
+    </div>
+</div>
+
+
+<div class="intro">
+
+    <h2>Browse to find and select your favourite movies</h2>
+</div>
+
+<div class="container filterBtn" id="myBtnContainer">
+    <h2>Sort</h2>
+    <button class="btn" onclick="filterSelection('all')">all</button>
+    <button class="btn" onclick="filterSelection('new')"> Newest</button>
+    <button class="btn" onclick="filterSelection('top')"> Highest Rating</button>
+    <button class="btn" onclick="filterSelection('top')"> Shortest</button>
+    <button class="btn" onclick="filterSelection('top')"> Trending</button>
+</div>
+
+<form action="/action_page.php">
+    <input type="checkbox" id="filter1" name="Filter1" value="all">
+    <label for="vehicle1"> All</label><br>
+    <input type="checkbox" id="vehicle2" name="vehicle2" value="Comedy">
+    <label for="vehicle2"> Comedy</label><br>
+    <input type="checkbox" id="vehicle3" name="vehicle3" value="Drama">
+    <label for="vehicle3"> Drama</label><br><br>
+    <input type="checkbox" id="vehicle3" name="vehicle3" value="Action">
+    <label for="vehicle3"> action</label><br><br>
+    <input type="submit" value="search">
+</form>
+
+<div class="container">
+
+    <div class="box">
+        <div class="imgBox">
+            <img src="img/browse1.jpg" alt="picture 1" width="250px" height="300px">
+        </div>
+        <div class="details">
+            <div class="content">
+                <h2>Infinity War</h2>
+                <p>Marvel - Action</p>
+            </div>
+            <button class="btn btn1">Star</button>
+            <button class="btn btn2">Comment</button>
+        </div>
+    </div>
+
+
+    <div class="box">
+        <div class="imgBox">
+            <img src="img/browse2.jpg" alt="picture 2" width="250px" height="300px">
+        </div>
+        <div class="details">
+            <div class="content">
+                <h2>Mulan</h2>
+                <p>Disney - Action</p>
+            </div>
+            <button class="btn btn1">Star</button>
+            <button class="btn btn2">Comment</button>
+        </div>
+    </div>
+
+
+    <footer class="footer-distributed">
+
+        <div class="footer-left">
+
+            <h3 class="logoFooter">VELP</h3>
+
+            <p class="footer-links textFooter">
+                <a href="index.html">Home</a>
+                ·
+                <a href="favourite.html">Favourites</a>
+                ·
+                <a href="account.html">Account</a>
+                ·
+
+            </p>
+
+            <p class="footer-company-name logoFooter">VELP &copy; 2019</p>
+        </div>
+
+        <div class="footer-center textFooter">
+
+            <div>
+                <i class="fa fa-map-marker"></i>
+                <p><span>8884 140B Street</span>Surrey, BC, Canada</p>
+            </div>
+
+            <div>
+                <i class="fa fa-phone"></i>
+                <p>+1(604)-240-1471</p>
+            </div>
+
+            <div>
+                <i class="fa fa-envelope"></i>
+                <p><a href="mailto:shyamagalabadage@gmail.com" class="emailColor">info@velp.com</a></p>
+            </div>
+
+        </div>
+
+        <div class="footer-right textFooter">
+
+            <p class="footer-company-about">
+                <span>About us</span>
+                velp, the ultimate movie review website where you can select your favourite movie and review them
+
+            </p>
+
+            <div class="footer-icons">
+
+                <a href="https://www.facebook.com/groups/98439537650"><i class="fa fa-facebook"></i></a>
+                <a href="https://twitter.com/nperera17?s=20"><i class="fa fa-twitter"></i></a>
+                <a href="https://ca.linkedin.com/in/nisha-perera-667b54130?trk=people-guest_people_search-card"><i
+                        class="fa fa-linkedin"></i></a>
+
+
+            </div>
+
+        </div>
+
+    </footer>
+
+
+</body>
+
+</html>
+
+<!-- CITATIONS FOR IMAGES USED -->
+<!-- IMAGES TAKEN FROM
+https://www.zairazarotti.com/on-the-beauty-and-restlessness-of-solitude-a-layered-walnut-cake-with-italian-meringue-mascarpone-frosting-figs-and-salty-caramel/
+
+https://www.justdial.com/Indore/Devs-Bakery-HIG-Colony/0731PX731-X731-140715132725-K2S4_BZDET
+
+https://www.etsy.com/ca/listing/675734109/wedding-cake-topper-mr-and-mrs-hexagon
+
+https://thefeedfeed.com/thelittleplantation/vegan-rhubarb-layer-cake
+
+https://www.thekitchenmccabe.com/2018/12/06/paleo-caramel-custard-cake-toasted-meringue
+/ -->
